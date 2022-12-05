bd738d49ef399db877bb858ba442f0af4b07143d
David Gustavo De la Rocha Ramirez <daviddelarocha.dev@gmail.com>
2022-12-06 00:00:03 +0100

    bt connection validatedd

--- conne_bt.py
+++ conne_bt.py
@@ -3,18 +3,15 @@ Projet: RobAFIS
 Author: David Gustavo DE LA ROCHA RAMIREZ
 """
 import sys
-from bluepy.btle import UUID, Peripheral
+from bluepy.btle import Peripheral
 
-MAC_ADDR = "70:3E:97:93:14:FF"
+MAC_ADDR = "00:1B:10:66:6D:EB"
 
-if len(sys.argv) != 2:
-  print("Fatal, must pass device address:", sys.argv[0], "<device address="">")
-  quit()
+class BluetoothConnector(Peripheral):
+	"""
+	...
+	"""
+	def __init__(self) -> None:
+		super().__init__()
 
-p = Peripheral(sys.argv[1],"random")
-
-services=p.getServices()
-
-#displays all services
-for service in services:
-   print(service)
+	
